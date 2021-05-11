8d5ebc8f885a2225d561983cb525c24a04b206af
stanishevskaya.lena <stanishevskaya.lena@gmail.com>
2021-05-11 12:43:59 +0300

    lab 1

---
+++ lab1/MatrixService.h
@@ -0,0 +1,29 @@
+#pragma once
+#include <stdlib.h>
+#include <time.h>
+#include <emmintrin.h>
+
+#define MATRIX_HEIGHT	48
+#define MATRIX_WIDTH 	48
+
+#define CELL_HEIGHT	100
+#define CELL_WIDTH 	100
+
+// Maximum value that can be returned by the rand function:
+#define RAND_MAX 0x7fff
+
+typedef double T;
+
+class MatrixService {
+public:
+	MatrixService() {}
+	~MatrixService() {}
+
+	T**** allocation(int, int, int, int);
+	bool generateValues(T****, int, int, int, int);
+	T**** multiply(T****, T****, int, int, int, int);
+	T**** multiplyNotVectorized(T****, T****, int, int, int, int);
+	T**** multiplySSE(T****, T****, int, int, int, int);
+	bool equals(T****, T****, int, int, int, int);
+	bool destroy(T****, int, int, int, int);
+};
